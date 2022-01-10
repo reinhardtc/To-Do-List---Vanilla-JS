071ce35398e598e8430130f63d5f8a785191c763
reinhardtc <reinhardtc22@gmail.com>
2022-01-10 11:14:30 -0600

    initial file structure and basic html layout

---
+++ script.js
@@ -0,0 +1,4 @@
+// get DOM elements by class
+const form = document.getElementsByClassName('form');
+const input = document.getElementsByClassName('inputBox');
+const btn = document.getElementsByClassName('submitBtn');
