a7198379e345b7e07566b7379a9195e34cfbec62
reinhardtc <reinhardtc22@gmail.com>
2022-01-10 14:20:13 -0600

    basic functionality

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 // get DOM elements by class
-const form = document.getElementsByClassName('form');
-const input = document.getElementsByClassName('inputBox');
-const btn = document.getElementsByClassName('submitBtn');
+const toDoData = [];
+const form = document.querySelector('form');
+const input = document.querySelector('[name="toDo"]');
+const toDoList = document.getElementById('toDoList');
+
+//update internal HTML with new list item
+function updateToDoList(toDoItem) {
+  toDoData.push(toDoItem);
+  const list = document.createElement('li');
+  list.innerHTML = toDoItem;
+  toDoList.appendChild(list);
+  localStorage.setItem('toDos', JSON.stringify(toDoData));
+}
+
+form.onsubmit = event => {
+  event.preventDefault();
+  updateToDoList(input.value);
+};
+//save to local storage
